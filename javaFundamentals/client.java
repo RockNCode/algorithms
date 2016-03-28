d243d5013598eb038272bccac3f08790d286b304
Manuel Garcia <alexgarcia2k@gmail.com>
2016-03-28 09:05:40 -0700

    Some java fundamental examples.

---
+++ javaFundamentals/client.java
@@ -0,0 +1,9 @@
+public class client{
+
+    public static void main(String[] args){
+	exampleImplInt ex = new exampleImplInt();
+	System.out.println("Result of interface example is " + ex.foo());
+
+    }
+}
+
---
+++ javaFundamentals/exampleImplInt.java
@@ -0,0 +1,8 @@
+public class exampleImplInt implements InterfaceExample{
+    
+
+    @Override
+    public boolean foo(){
+	return true;
+    }
+}
